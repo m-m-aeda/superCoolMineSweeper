203dd103cceb27c6133c8ed50d77e0b701d5a8b9
前田光貴 <mitsukim@maedakoukinoiMac.local>
2017-08-09 21:08:19 +0900

    いりそうなやつを

---
+++ SupreCoolMineSweeper/app/src/main/java/com/goodboy/manoma/suprecoolminesweeper/minesweeper/Entity.java
@@ -0,0 +1,10 @@
+package com.goodboy.manoma.suprecoolminesweeper.minesweeper;
+
+/**
+ * エンティティ
+ */
+public abstract class Entity {
+    static long createIdentity() {
+        long identity = System.
+    }
+}
